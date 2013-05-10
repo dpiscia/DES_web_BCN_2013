888591ea33a7f528eb9fa360cca3b51dc0e5683d
Davide <davidepi79@gmail.com>
2013-05-10 13:04:47 +0200

    sqlalchemy pyramid scaffold

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'SQLAlchemy',
+    'transaction',
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='MyProject',
+      version='0.1',
+      description='DES_BCN_2013',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='Pau_team',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='myproject',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = myproject:main
+      [console_scripts]
+      initialize_MyProject_db = myproject.scripts.initializedb:main
+      """,
+      )
