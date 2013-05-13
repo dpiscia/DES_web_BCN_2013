b41fea1ab2bf71fff55f294e010876a33eb8516d
Davide <davidepi79@gmail.com>
2013-05-13 12:14:39 +0200

    add:

--- DES_BCN/des_bcn/__init__.py
+++ DES_BCN/des_bcn/__init__.py
@@ -1,20 +1,23 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
-from .models import (
-    DBSession,
-    Base,
-    )
+
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    engine = engine_from_config(settings, 'sqlalchemy.')
-    DBSession.configure(bind=engine)
-    Base.metadata.bind = engine
+
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
+    
+    config.add_route('program', '/program')
     config.add_route('home', '/')
+    config.add_route('participants', '/participants')
+    config.add_route('venue_accommodation', '/venue_accommodation')
+    config.add_route('travel', '/travel')
+    config.add_route('registration', '/registration')
+    config.add_route('contact', '/contact')
+    
     config.scan()
     return config.make_wsgi_app()
